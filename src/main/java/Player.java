3219795d8a1ef5239cc63bc032c895c99652a876
Michal Sladecek <469434@mail.muni.cz>
2021-04-07 12:23:10 +0200

    Added player class

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Player {
+import java.util.ArrayList;
+
+public class Player {
+    Coordinates location = new Coordinates(40,40);
+    Direction direction = Direction.RIGHT;
+    ArrayList<Coordinates> path = new ArrayList<Coordinates>();
+
+    public void updatePath(){
+        path.add(new Coordinates(location));
+    }
+
+
+    enum Direction{
+        UP,
+        RIGHT,
+        DOWN,
+        LEFT
+    }
+
+
 }

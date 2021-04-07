03b27162c5f3d487c3b01a14bf17de0b5038ee29
Michal Sladecek <469434@mail.muni.cz>
2021-04-07 12:25:27 +0200

    Added movePlayer into player class

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -5,6 +5,39 @@ public class Player {
     Direction direction = Direction.RIGHT;
     ArrayList<Coordinates> path = new ArrayList<Coordinates>();
 
+    public void movePlayer(int moveAmount, int gameWidth, int gameHeight) {
+        switch(direction){
+            case UP:
+                if (location.getY()>0){
+                    location.move(0,-moveAmount);
+                } else {
+                    location.setY(gameHeight);
+                }
+                break;
+            case RIGHT:
+                if (location.getX() < gameWidth){
+                    location.move(moveAmount,0);
+                } else {
+                    location.setX(0);
+                }
+                break;
+            case DOWN:
+                if (location.getY() < gameHeight){
+                    location.move(0,moveAmount);
+                } else {
+                    location.setY(0);
+                }
+                break;
+            case LEFT:
+                if (location.getX()>0){
+                    location.move(-moveAmount,0);
+                } else {
+                    location.setX(gameWidth);
+                }
+                break;
+        }
+    }
+
     public void updatePath(){
         path.add(new Coordinates(location));
     }

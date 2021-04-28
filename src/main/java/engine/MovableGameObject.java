6cdbd08a03dba2c48e7e504ff761be9f434710b5
AdamRepasky <57711080+AdamRepasky@users.noreply.github.com>
2021-04-28 21:32:36 +0200

    Moved TronPlayer move methods to parent class

--- src/main/java/engine/MovableGameObject.java
+++ src/main/java/engine/MovableGameObject.java
@@ -4,10 +4,12 @@ import java.awt.*;
 
 public abstract class MovableGameObject extends GameObject {
 
+    protected Core core;
     protected Direction direction;
 
-    public MovableGameObject(Coordinates location, Color color) {
+    public MovableGameObject(Coordinates location, Color color, Core core) {
         super(location, color);
+        this.core = core;
     }
 
     public void setDirectionUP(){
@@ -34,6 +36,42 @@ public abstract class MovableGameObject extends GameObject {
             direction = Direction.valueOf(3);
         } else {
             this.direction = Direction.valueOf((this.direction.getValue() - 1) % 4);
+        }
+    }
+
+    private void move(int dx, int dy){
+        this.location = new Coordinates(location.getX()+dx,location.getY()+dy);
+    }
+
+    public void movePlayer(int moveAmount) {
+        switch(direction){
+            case UP:
+                move(0,-moveAmount);
+                break;
+            case RIGHT:
+                move(moveAmount,0);
+                break;
+            case DOWN:
+                move(0,moveAmount);
+                break;
+            case LEFT:
+                move(-moveAmount,0);
+                break;
+        }
+    }
+
+    public void correctOutOfBounds() {
+        if (location.getY() < 0){
+            location.setY(core.getGameHeight());
+        }
+        if (location.getX() > core.getGameWidth()){
+            location.setX(0);
+        }
+        if (location.getY() > core.getGameHeight()){
+            location.setY(0);
+        }
+        if (location.getX() < 0){
+            location.setX(core.getGameWidth());
         }
     }
 }
--- src/main/java/tron/TronPlayer.java
+++ src/main/java/tron/TronPlayer.java
@@ -9,62 +9,18 @@ public class TronPlayer extends MovableGameObject {
     public static int MOVE_AMOUNT = 5;
     ArrayList<Coordinates> path = new ArrayList<>();
 
-    private Core game;
-
-    public TronPlayer(Coordinates location, Direction direction, Color color, Core engine) {
-        super(location, color);
+    public TronPlayer(Coordinates location, Direction direction, Color color, Core core) {
+        super(location, color, core);
         this.direction = direction;
-        this.game = engine;
-    }
-
-    private void move(int dx, int dy){
-        this.location = new Coordinates(location.getX()+dx,location.getY()+dy);
-    }
-
-    public void correctOutOfBounds() {
-        if (location.getY() < 0){
-            location.setY(game.getGameHeight());
-        }
-        if (location.getX() > game.getGameWidth()){
-            location.setX(0);
-        }
-        if (location.getY() > game.getGameHeight()){
-            location.setY(0);
-        }
-        if (location.getX() < 0){
-            location.setX(game.getGameWidth());
-        }
-    }
-
-    public void movePlayer(int moveAmount) {
-        switch(direction){
-            case UP:
-                move(0,-moveAmount);
-                break;
-            case RIGHT:
-                move(moveAmount,0);
-                break;
-            case DOWN:
-                move(0,moveAmount);
-                break;
-            case LEFT:
-                move(-moveAmount,0);
-                break;
-        }
     }
 
     public boolean checkCollision(GameObject other){
-        if (other.getLocation().equals(location)) {
-            return true;
-        }
-        return false;
+        return other.getLocation().equals(location);
     }
-
-
 
     @Override
     public void update() {
-        game.addGameObject(new TronPlayerPathBlock(new Coordinates(location),color));
+        this.core.addGameObject(new TronPlayerPathBlock(new Coordinates(location),color));
         movePlayer(MOVE_AMOUNT);
         correctOutOfBounds();
     }

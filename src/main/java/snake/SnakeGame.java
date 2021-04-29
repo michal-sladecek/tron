b0aefe466c00c686c55f0b4b968803e45de49ff6
AdamRepasky <57711080+AdamRepasky@users.noreply.github.com>
2021-04-29 20:37:41 +0200

    generateApples method created

--- src/main/java/snake/SnakeGame.java
+++ src/main/java/snake/SnakeGame.java
@@ -19,15 +19,19 @@ public class SnakeGame extends Core {
         SnakeHead p1 = new SnakeHead(new Coordinates(40,40), Direction.RIGHT, Color.green,this);
 
         gameObjectManager.addGameObject(p1);
+        generateApples();
+
+
+        presentation.addGameControl(new SnakeController(p1, KeyEvent.VK_UP,KeyEvent.VK_DOWN, KeyEvent.VK_RIGHT, KeyEvent.VK_LEFT));
+    }
+
+    private void generateApples() {
         for (int i = 0; i < 100; i++) {
             int randomX = ThreadLocalRandom.current().nextInt(0, this.getGameWidth()  / SnakeHead.MOVE_AMOUNT + 1) * SnakeHead.MOVE_AMOUNT;
             int randomY = ThreadLocalRandom.current().nextInt(0, this.getGameHeight() / SnakeHead.MOVE_AMOUNT + 1) * SnakeHead.MOVE_AMOUNT;
             Apple apple1 = new Apple(new Coordinates(randomX,randomY), Color.red);
             gameObjectManager.addGameObject(apple1);
         }
-
-
-        presentation.addGameControl(new SnakeController(p1, KeyEvent.VK_UP,KeyEvent.VK_DOWN, KeyEvent.VK_RIGHT, KeyEvent.VK_LEFT));
     }
 
     public static void main(String[] args) {
